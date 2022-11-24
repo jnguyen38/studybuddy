9e4bcf96a2f926d5f0d9b4e3d9f36c4e2675c59e
Jonathan Nguyen <jnguyen5@nd.edu>
2022-11-23 20:32:11 -0500

    Permissions modifications, deploy script update, api/get/majors, api/get/emails, api/get/usernames, api/put/signin, api/post/signup, api/post/location

--- server/index.js
+++ server/index.js
@@ -4,56 +4,84 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const  PORT = 5001;
+const  PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/get/study-location", (req, res) => {
-    db.query("SELECT spot_id, building, location \
-                FROM study_spots", (err, result) => {
-        if (err) console.log(err)
-        res.send(result)
-    })
-});
+/* GET API ENDPOINTS */
 
 app.get("/api/get", (req, res) => {
     db.query("SELECT * \
                 FROM study_spots", (err, result) => {
-        if (err) console.log(err)
-        res.send(result)
+        if (err) console.log(err);
+        res.send(result);
     });
 });
 
-app.post("/api/post/admin", (req, res) => {
-    db.query(`SELECT admin \
+app.get("/api/get/majors", (req, res) => {
+    db.query("SELECT major \
+                FROM majors \
+                ORDER BY major;", (err, result) => {
+        if (err) console.log(err);
+        res.send(result);
+    });
+});
+
+app.get("/api/get/usernames", (req, res) => {
+    db.query("SELECT username \
+                FROM users;", (err, result) => {
+        if (err) console.log(err);
+        res.send(result);
+    });
+});
+
+app.get("/api/get/emails", (req, res) => {
+    db.query("SELECT email \
+                FROM users;", (err, result) => {
+        if (err) console.log(err);
+        res.send(result);
+    });
+});
+
+/* PUT API ENDPOINTS */
+
+app.put("/api/put/edit", (req, res) => {
+    db.query(`UPDATE study_spots \
+                SET ${req.body.query}=\"${req.body.description}\" \
+                WHERE spot_id=\"${req.body.id}\"`,(err, result) => {
+        if (err) console.log(err);
+        res.send(result);
+    });
+});
+
+app.put("/api/put/signin", (req, res) => {
+    db.query(`SELECT first_name, last_name, username, admin \
                 FROM users \
                 WHERE username=\"${req.body.user}\" and password=\"${req.body.password}\"`, (err, result) => {
-        if (err) console.log(err)
+        if (err) console.log(err);
 
+        let user = {isSignedIn: false, isAdmin: false, firstName: "", lastName: "", username: ""};
         if (result.length) {
-            if (result[0].admin) res.send(true)
-            else res.send(false)
-        } else {
-            res.send(false)
+            db.query(`UPDATE users SET last_login=now(), latitude=${req.body.latitude}, longitude=${req.body.longitude} WHERE username=\"${req.body.user}\"`);
+            user.isSignedIn = true;
+            user.isAdmin = (result[0].admin) ? true : false;
+            user.firstName = result[0].first_name;
+            user.lastName = result[0].last_name;
+            user.username = result[0].username;
         }
+
+        res.send(user);
 
     });
 });
+
+/* POST API ENDPOINTS */
 
 app.post("/api/post/review", (req, res) => {
     db.query(`INSERT INTO reviews (time, date, content, name, rating, space_id) \
-                VALUES (now(), curdate(), \"${req.body.content}\", \"${req.body.name}\", ${req.body.rating}, ${req.body.space_id})`, (err, result) => {
-        if (err) console.log(err)
-        res.send(result)
-    });
-});
-
-app.post("/api/post/editDesc", (req, res) => {
-    db.query(`UPDATE study_spots \
-                SET description=\"${req.body.description}\" \
-                WHERE spot_id=\"${req.body.id}\"`,(err, result) => {
-        if (err) console.log(err)
-        res.send(result)
+                VALUES (now(), curdate(), \"${req.body.description}\", \"${req.body.name}\", ${req.body.rating}, ${req.body.spot_id})`, (err, result) => {
+        if (err) console.log(err);
+        res.send(result);
     });
 });
 
@@ -65,19 +93,33 @@ app.post("/api/post/search", (req, res) => {
     let naturalLight = (req.body.naturalLight) ? `natural_light_rating=${req.body.naturalLight}` : `natural_light_rating>=1`;
     let capacity = (req.body.capacity) ? `max_capacity>=${req.body.capacity}` : `max_capacity>=1`;
     let group = (req.body.group) ? `max_group_size>=${req.body.group}` : `max_group_size>=1`;
-    console.log(building)
-    console.log(outlets)
-    console.log(loudness)
-    console.log(naturalLight)
-    console.log(capacity)
-    console.log(group)
+
     db.query(`SELECT * \
                 FROM study_spots \
                 WHERE ${building} AND ${outlets} AND ${loudness} AND ${naturalLight} AND ${group} AND ${capacity}`, (err, result) => {
-        if (err) console.log(err)
-        res.send(result)
-    })
+        if (err) console.log(err);
+        res.send(result);
+    });
 });
+
+app.post("/api/post/location", (req, res) => {
+    db.query(`SELECT * \
+                FROM study_spots \
+                WHERE spot_id="${req.body.spot_id}"`, (err, result) => {
+        if (err) console.log(err);
+        res.send(result);
+    });
+});
+
+app.post("/api/post/signup", (req, res) => {
+	db.query(`INSERT INTO users (first_name, last_name, email, major, latitude, longitude, last_login, created, username, password, admin) \
+                VALUES (\"${req.body.first_name}\", \"${req.body.last_name}\", \"${req.body.email}\", \"${req.body.major}\", ${req.body.latitude}, ${req.body.longitude}, now(), now(), \"${req.body.username}\", \"${req.body.password}\", 0)`, (err, result) => {
+        if (err) console.log(err);
+        res.send(result);
+    });
+});
+
+/* LISTENER */
 
 app.listen(PORT, ()=>{
     console.log("Server is running on port " + PORT)
